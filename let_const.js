2052e0ccb5bb05394827d6eefae9fb99fd8a47f0
asifhossain2k20 <59142263+asifhossain2k20@users.noreply.github.com>
2021-05-21 02:09:45 +0600

    first commit

---
+++ let_const.js
@@ -0,0 +1,31 @@
+//const Declaration
+
+const name="Asif Hossain";
+console.log(name);
+
+//const in Array
+
+const numbers=[10,11,12];
+numbers[1]=99;
+numbers.push(188);
+console.log(numbers);
+
+//const in object
+
+const nayok={name:"akash",phone:018888 };
+console.log(nayok);
+
+//let declaration
+
+let patientName="Alif";
+patientName="Adib";
+console.log(patientName);
+
+//let is scope variable
+
+let sum=0;
+for(let i=0;i<10;i++){
+    sum+=i;
+}
+console.log(sum);
+
