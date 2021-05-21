91ec13470370467375a88af816601d7bba770bac
asifhossain2k20 <59142263+asifhossain2k20@users.noreply.github.com>
2021-05-21 12:30:47 +0600

    string template done

---
+++ string_template.js
@@ -0,0 +1,16 @@
+//string template
+
+const firstName="Alif";
+const lastName="hossain";
+
+const fullName=`${firstName} ${lastName} ${10+20+30} He got This Number`
+
+console.log(fullName);
+
+//string template creating multiline in caret
+
+const multiLine=`I am Alif
+I Read in BAUST
+I am Loving Coding`
+
+console.log(multiLine);
